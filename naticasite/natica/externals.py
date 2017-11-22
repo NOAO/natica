ea42789b44b53927a85d5f8470a0e8ee48148623
Steve Pothier <pothiers@gmail.com>
2017-11-22 09:57:21 -0700

    *** empty log message ***

---
+++ naticasite/natica/externals.py
@@ -0,0 +1,8 @@
+"""Provides a library of calls made from external references (i.e. not
+from a http request). Allows other apps to access to app functionality
+without going through service.
+
+Changes to anything here may REQUIRE CORRESPONDING CHANGE in the
+calling routing.
+"""
+
